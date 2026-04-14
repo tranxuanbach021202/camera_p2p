7fb859bd5fe332682af7a1ddf1acad866d64f04c
Bach Xuan <bachtx@ntlsoft.com.vn>
2026-04-14 09:13:31 +0700

    Initial Commit

---
+++ app_camera_p2p_v1/app_camera_p2p_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  app_camera_p2p_v1App.swift
+//  app_camera_p2p_v1
+//
+//  Created by Bach Xuan on 14/4/26.
+//
+
+import SwiftUI
+
+@main
+struct app_camera_p2p_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
